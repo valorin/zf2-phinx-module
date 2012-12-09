f419984d423b9ac2836e9b854368e2df5f5c877c
Stephen Rees-Carter <stephen@rees-carter.net>
2012-12-10 07:44:14 +1100

    CLI output how shows Phinx version.

--- Module.php
+++ Module.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * ISLE - Intelligent Shopping List gEnerator
+ *
+ * Copyright (c) 2012, Stephen Rees-Carter - stephen@rees-carter.net
+ * http://stephen.rees-carter.net/
+ *
+ * All rights reserved. Unauthorised distribution is prohibited.
+ */
 
 namespace PhinxModule;
 
@@ -12,12 +20,72 @@ use Zend\ModuleManager\Feature\ConfigProviderInterface;
 class Module implements
     AutoloaderProviderInterface,
     ConfigProviderInterface,
+    ConsoleBannerProviderInterface,
     ConsoleUsageProviderInterface
 {
     /**
      * @var string
      */
     const VERSION = '0.0.1';
+
+
+    /**
+     * Generates the Console Banner text
+     *
+     * @param  Console $console
+     * @return String
+     */
+    public function getConsoleBanner(Console $console)
+    {
+        /**
+         * Work out Phinx version
+         */
+        $file    = false;
+        $version = "unknown";
+        if (file_exists(__DIR__ ."/../../bin/phinx")) {
+            $file = file_get_contents(__DIR__ ."/../../bin/phinx");
+        } elseif (file_exists(__DIR__ ."/../../vendor/bin/phinx")) {
+            $file = file_get_contents(__DIR__ ."/../../vendor/bin/phinx");
+        }
+
+        if ($file && preg_match("/(\d+\.\d+\.\d+)/", $file, $match)) {
+            $version = "v".$match[1];
+        }
+
+
+        /**
+         * Output version
+         */
+        $status = $console->colorize("=> ", Colour::GREEN)
+                 .$console->colorize("Phinx ", Colour::CYAN)
+                 .$console->colorize($version, Colour::MAGENTA)
+                 ."\n"
+                 .$console->colorize("=> ", Colour::GREEN)
+                 .$console->colorize("Phinx module ", Colour::CYAN)
+                 .$console->colorize("v".self::VERSION, Colour::MAGENTA);
+
+        return $status;
+    }
+
+
+    /**
+     * Define Console Help text
+     *
+     * @param  Console $console
+     * @return String
+     */
+    public function getConsoleUsage(Console $console)
+    {
+        return array(
+            $console->colorize('Phinx module commands', Colour::CYAN),
+            $console->colorize('phinx sync', Colour::GREEN)
+                => "Sync application database credentials with Phinx.",
+            $console->colorize('phinx help', Colour::GREEN)
+                => "List the Phinx console usage information.",
+            $console->colorize('phinx <commands>', Colour::GREEN)
+                => "Run the specified Phinx command (run 'phinx help' for the commands list).",
+        );
+    }
 
 
     public function getAutoloaderConfig()
@@ -37,32 +105,5 @@ class Module implements
     public function getConfig($env = null)
     {
         return include __DIR__ . '/config/module.config.php';
-    }
-
-    public function getConsoleBanner(Console $console){
-
-        $status = $console->colorize("=> ", Colour::GREEN)
-                 .$console->colorize("Phinx module ", Colour::CYAN)
-                 .$console->colorize("v".self::VERSION, Colour::MAGENTA);
-
-        return $status;
-    }
-
-    /**
-     * Define Console Help text
-     *
-     * @return Array
-     */
-    public function getConsoleUsage(Console $console)
-    {
-        return array(
-            $console->colorize('Phinx module commands', Colour::CYAN),
-            $console->colorize('phinx sync', Colour::GREEN)
-                => "Sync application database credentials with Phinx.",
-            $console->colorize('phinx help', Colour::GREEN)
-                => "List the Phinx console usage information.",
-            $console->colorize('phinx <commands>', Colour::GREEN)
-                => "Run the specified Phinx command (run 'phinx help' for the commands list).",
-        );
     }
 }
